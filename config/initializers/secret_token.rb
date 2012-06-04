bb820370e9719693b8096f7ee2ff62c72e99e3df
George Pasley <gpasley@3freed.com>
2012-06-03 23:35:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bankvendors::Application.config.secret_token = '08faf0f8ba4a17ff01a3382405f722da7262036cb91db5259297e4b1fbb99b1646bb0d18dfc17c0997f6ffdec4f6501504f6863cb186494b7dcb0837380bc04e'
